13ea80e0be108ff619c05eb0b7483d63725a3f58
Kevin Driscoll <driscollkevin@gmail.com>
2013-10-07 00:54:10 -0700

    Added some new error messages

--- pipermail/extract_urls_from_pipermail.py
+++ pipermail/extract_urls_from_pipermail.py
@@ -12,10 +12,14 @@ import csv
 import datetime
 import requests
 import subprocess
+import sys
+import time
+
 
 # INSANE URI matching regex from:
 # http://daringfireball.net/2010/07/improved_regex_for_matching_urls
 URL_RE = bs.re.compile(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?��....]))')
+
 
 def parse_first_line(line):
     email, timestamp = line.strip()[5:].split('  ', 1)
@@ -43,46 +47,59 @@ def iter_emails(f):
             text += line
     yield (email, dt, subject, text)
 
-piperurl = u'http://www.classiccmp.org/pipermail/cctalk/'
 
-print "Requesting", piperurl
-r = requests.get(piperurl)
-soup = bs.BeautifulSoup(r.text)
+if __name__=="__main__":
 
-listname = soup.find('h1').text
-print "Found list named:", listname
+    if len(sys.argv) < 2:
+        print "Please specify a pipermail archive URL."
+        sys.exit(1)
+    else:
+        piperurl = sys.argv[1]
 
-gzips = []
-for row in soup.findAll('tr')[1:]:
-    gzips.append(row.findAll('td')[2].find('a')['href'])
-print "Found", len(gzips), "months of archives."
+    print "Requesting", piperurl
+    r = requests.get(piperurl)
+    if r.ok:
+        soup = bs.BeautifulSoup(r.text)
+    else:
+        print "Error code", r.status_code
+        sys.exit(1)
 
-rows = [('url', 'subject', 'from', 'timestamp')]
-for fn in gzips:
-    print "Downloading", fn
-    r = requests.get(piperurl + fn)
-    with open(fn, 'w') as fp:
-        for chunk in r.iter_content(1024):
-            fp.write(chunk)
-    subprocess.check_call(['gunzip', '-fqk', fn])
-    fn = fn.rsplit('.', 1)[0]
-    with open(fn, 'r') as fp:
-        count = 0
-        for email, dt, subject, text in iter_emails(fp):
-            for u in URL_RE.finditer(text):
-                url = u.group()
-                rows.append((url, subject, email, dt.isoformat()))
-                count += 1
-        print "Found", count, "urls."
+    listname = soup.find('h1').text
+    print "Found list named:", listname
 
-print "Found", len(rows[1:]), "in total."
+    gzips = []
+    for row in soup.findAll('tr')[1:]:
+        gzips.append(row.findAll('td')[2].find('a')['href'])
+    print "Found", len(gzips), "months of archives."
 
-output_fn = '{0}_links.csv'.format(listname.replace(' ', '_'))
-print "Writing to", output_fn
-with open(output_fn, 'w') as f:
-    csvw = csv.writer(f, dialect="excel")
-    csvw.writerows(rows)
-print "Bye-bye!"
+    rows = [('url', 'subject', 'from', 'timestamp')]
+    for fn in gzips:
+        print "Downloading", fn
+        r = requests.get(piperurl + fn)
+        with open(fn, 'w') as fp:
+            for chunk in r.iter_content(1024):
+                fp.write(chunk)
+        subprocess.check_call(['gunzip', '-fqk', fn])
+        fn = fn.rsplit('.', 1)[0]
+        with open(fn, 'r') as fp:
+            count = 0
+            for email, dt, subject, text in iter_emails(fp):
+                for u in URL_RE.finditer(text):
+                    url = u.group()
+                    rows.append((url, subject, email, dt.isoformat()))
+                    count += 1
+            print "Found", count, "urls."
+        print "Sleeping for 10s..."
+        time.sleep(10)
+
+    print "Found", len(rows[1:]), "in total."
+
+    output_fn = '{0}_links.csv'.format(listname.replace(' ', '_'))
+    print "Writing to", output_fn
+    with open(output_fn, 'w') as f:
+        csvw = csv.writer(f, dialect="excel")
+        csvw.writerows(rows)
+    print "Bye-bye!"
 
 
 
